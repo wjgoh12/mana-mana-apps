838b03d5074dacae38ecafa7373ee7cfffe47f33
Wen_Jing <wjgoh@dynafront.com>
2025-08-19 17:10:19 +0800

    add API endpoint

--- lib/provider/api_endpoint.dart
+++ lib/provider/api_endpoint.dart
@@ -1,13 +1,21 @@
-class ApiEndpoint{
+class ApiEndpoint {
   static const String ownerUserData = '/mobile/dash/owners';
   static const String ownerUnit = '/mobile/dash/units';
   static const String downloadPdfStatement = '/owners/statement/mobile';
-  static const String downloadPdfAnnualStatement = '/owners/statement/annual/mobile';
+  static const String downloadPdfAnnualStatement =
+      '/owners/statement/annual/mobile';
   static const String dashboardTotalByMonth = '/mobile/dash/total_bymonth';
   static const String dashboardReveueByYear = '/mobile/dash/total_byyear';
-  static const String getUnitByMonth = '/mobile/dash/total_bymonth_single_type_unit';
+  static const String getUnitByMonth =
+      '/mobile/dash/total_bymonth_single_type_unit';
   static const String locationByMonth = '/mobile/dash/total_bymonth_location';
   static const String userLogout = '/mobile/dash/refs/logout';
-  static const String propertyContractType ='/mobile/enqs/getPropertyOverviewContractType';
-  static const String propertyOccupancyRate = '/mobile/enqs/propertyOverviewOccupancyRate';
+  static const String propertyContractType =
+      '/mobile/enqs/getPropertyOverviewContractType';
+  static const String propertyOccupancyRate =
+      '/mobile/enqs/propertyOverviewOccupancyRate';
+  static const String getCalendarBlockDate =
+      '/mobile/enqs/getCalendarBlockedDates';
+  static const String getAllRedemptionDate =
+      '/mobile/enqs/getAllRedemptionRate ';
 }
