c18b85a25d790c3cbf15a82a4cbc38927ceb1bb6
Wen_Jing <wjgoh@dynafront.com>
2025-09-24 14:17:08 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:mana_mana_app/config/env_config.dart';
 import 'package:mana_mana_app/screens/New_Dashboard/ViewModel/new_dashboardVM.dart';
+import 'package:mana_mana_app/provider/global_data_manager.dart';
 import 'package:mana_mana_app/splashscreen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  // change dev and prod
+  // Initialize the app
+  WidgetsFlutterBinding.ensureInitialized();
   initApp('prod');
-  runApp(ChangeNotifierProvider(
-    create: (context) => NewDashboardVM()..fetchData(),
-    child: const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Splashscreen(),
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => NewDashboardVM()..fetchData(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => GlobalDataManager(),
+        ),
+      ],
+      child: const MyApp(),
     ),
-  ));
+  );
 }
 
 Future<void> initApp(String env) async {
@@ -26,60 +35,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const Splashscreen(), // Set initial route
     );
   }
 }
